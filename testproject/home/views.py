5f0941a0a9355efa1d05b06819b4d2a629abda92
veryrichluxury <veryrichluxury@gmail.com>
2018-10-22 13:36:34 +0300

    search_sub_0.2_b

--- testproject/home/views.py
+++ testproject/home/views.py
@@ -1,10 +1,8 @@
-
-
-
 from django import forms
 
 import re
 import json
+import requests
 
 MY_CHOICES = (
     ('1', 'sub.'),
@@ -15,10 +13,10 @@ MY_CHOICES = (
 
 class ContactForm(forms.Form):
 
-    ipadres = forms.ChoiceField(choices=MY_CHOICES)
-    domainx = forms.CharField()
-    work_done1 = forms.CharField(required=False,disabled=True)
-    subd1 = forms.CharField(required=False,disabled=True)
+	ipadres = forms.ChoiceField(choices=MY_CHOICES)
+	domainx = forms.CharField()
+	work_done1 = forms.CharField(required=False,disabled=True)
+	subd1 = forms.CharField(required=False,disabled=True)
 
 
 
@@ -39,33 +37,35 @@ def save_subdomains(subdomain,output_file):
 def home(request):
 
 	if request.method == 'POST':
-		form = ContactForm(request.POST)
+		#form = ContactForm(request.POST)
+		domainx = request.POST.get("domainx")
 
 		#Если форма заполнена корректно, сохраняем все введённые пользователем значения
-		if form.is_valid():
+		#if form.is_valid():
 
-			domainx = form.cleaned_data['domainx']
-			subdomains = []
-			target = clear_url(domainx)
+		#domainx = form.cleaned_data['domainx']
+		subdomains = []
+		target = clear_url(domainx)
 
-			req = requests.get("https://crt.sh/?q=%.{d}&output=json".format(d=target))
+		req = requests.get("https://crt.sh/?q=%.{d}&output=json".format(d=target))
 
-			if req.status_code != 200:
-				work_done = "Error"
-				exit(1)
+		if req.status_code != 200:
+			work_done = "Error"
+			exit(1)
 
-			json_data = json.loads('[{}]'.format(req.text.replace('}{', '},{')))
+		json_data = json.loads('[{}]'.format(req.text.replace('}{', '},{')))
 
-			for (key,value) in enumerate(json_data):
-				subdomains.append(value['name_value'])
+		for (key,value) in enumerate(json_data):
+			subdomains.append(value['name_value'])
 
-			work_done ="TARGET: {d}".format(d=target)
+		work_done ="TARGET: {d}".format(d=target)
 
-			subdomains = sorted(set(subdomains))
+		subdomains = sorted(set(subdomains))
 
-			for subdomain in subdomains:
-				work_done ="[-]  {s}".format(s=subdomain)	
+		for subdomain in subdomains:
+			work_donex ="[-]  {s}".format(s=subdomain)
 
+		return HttpResponse("<h2>Domain target, {s}</h2>".format(s=subdomains),"<h2>Subdomains: , {0}</h2>".format(work_done))
 			#subject = form.cleaned_data['subject']
 			#sender = form.cleaned_data['sender']
 			#message = form.cleaned_data['message']
@@ -83,7 +83,6 @@ def home(request):
 	#		return render(request, 'landing/landing.html')
 	else:
 		#Заполняем форму
-		datat = "dsdsds"
 		domainx1 = forms.CharField()
 		form = ContactForm()
 			#Отправляем форму на страницу
