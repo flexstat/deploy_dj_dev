7d04dc9ca4d25e4b45918542a0dfc7a1f2aeea90
veryrichluxury <veryrichluxury@gmail.com>
2018-10-22 09:58:18 +0300

    search_sub_0.2_a

--- testproject/home/views.py
+++ testproject/home/views.py
@@ -16,8 +16,9 @@ MY_CHOICES = (
 class ContactForm(forms.Form):
 
     ipadres = forms.ChoiceField(choices=MY_CHOICES)
-    domain_tar = forms.CharField()
-    work_done = forms.CharField(required=False,disabled=True)
+    domainx = forms.CharField()
+    work_done1 = forms.CharField(required=False,disabled=True)
+    subd1 = forms.CharField(required=False,disabled=True)
 
 
 
@@ -34,66 +35,60 @@ def save_subdomains(subdomain,output_file):
 		f.write(subdomain + '\n')
 		f.close()
 
-def main():
-	
-	#banner()
-	#args = parse_args()
-
-	subdomains = []
-	target = clear_url(domain_tar.domain)
-	output = domain_tar.output
-
-	req = requests.get("https://crt.sh/?q=%.{d}&output=json".format(d=target))
-
-	if req.status_code != 200:
-		print("[X] Information not available!") 
-		exit(1)
-
-	json_data = json.loads('[{}]'.format(req.text.replace('}{', '},{')))
-
-	for (key,value) in enumerate(json_data):
-		subdomains.append(value['name_value'])
-
-	
-	# print("\n[!] ---- TARGET: {d} ---- [!] \n".format(d=target))
-
-	subdomains = sorted(set(subdomains))
-
-	for subdomain in subdomains:
-#		print("[-]  {s}".format(s=subdomain))
-		if output is not None:
-			save_subdomains(subdomain,output)
-
- #  print("\n\n[!]  Done. Have a nice day! ;).")
-
 
 def home(request):
 
 	if request.method == 'POST':
 		form = ContactForm(request.POST)
+
 		#Если форма заполнена корректно, сохраняем все введённые пользователем значения
 		if form.is_valid():
-			subject = form.cleaned_data['subject']
-			sender = form.cleaned_data['sender']
-			message = form.cleaned_data['message']
-			copy = form.cleaned_data['copy']
+
+			domainx = form.cleaned_data['domainx']
+			subdomains = []
+			target = clear_url(domainx)
+
+			req = requests.get("https://crt.sh/?q=%.{d}&output=json".format(d=target))
+
+			if req.status_code != 200:
+				work_done = "Error"
+				exit(1)
+
+			json_data = json.loads('[{}]'.format(req.text.replace('}{', '},{')))
+
+			for (key,value) in enumerate(json_data):
+				subdomains.append(value['name_value'])
+
+			work_done ="TARGET: {d}".format(d=target)
+
+			subdomains = sorted(set(subdomains))
+
+			for subdomain in subdomains:
+				work_done ="[-]  {s}".format(s=subdomain)	
+
+			#subject = form.cleaned_data['subject']
+			#sender = form.cleaned_data['sender']
+			#message = form.cleaned_data['message']
+			#copy = form.cleaned_data['copy']
                         #ipadres = forms.cleaned_data['ipadres']
-			recipients = ['bigcaches@ya.ru']
+			#recipients = ['bigcaches@ya.ru']
 			#Если пользователь захотел получить копию себе, добавляем его в список получателей
-			if copy:
-				recipients.append(sender)
-			try:
-				send_mail(subject, message, 'bigcaches@ya.ru', recipients)
-			except BadHeaderError: #Защита от уязвимости
-				return HttpResponse('block некорректный заголовок')
+	#		if copy:
+	#			recipients.append(sender)
+	#		try:
+	#			send_mail(subject, message, 'bigcaches@ya.ru', recipients)
+	#		except BadHeaderError: #Защита от уязвимости
+	#			return HttpResponse('block некорректный заголовок')
 			#Переходим на другую страницу, если сообщение отправлено
-			return render(request, 'landing/landing.html')
+	#		return render(request, 'landing/landing.html')
 	else:
 		#Заполняем форму
+		datat = "dsdsds"
+		domainx1 = forms.CharField()
 		form = ContactForm()
-	#Отправляем форму на страницу
-	return render(request, 'home/index.html', {'form': form})
-
+			#Отправляем форму на страницу
+	return render(request, 'home/index.html', {'form': form}) 
+#	return render(request, 'home/index.html', {'form': form}, locals())
 
 
 
